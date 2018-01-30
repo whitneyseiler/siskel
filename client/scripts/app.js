da6c84ab31101540f1b1824d75876272457d5db8
whitneyseiler <whitney.seiler@gmail.com>
2018-01-29 16:19:42 -0800

    Pass all Siskel tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,8 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+    var toggleLikeState = !this.get('like');
+    this.set('like', toggleLikeState);
   }
 
 });
@@ -15,13 +16,14 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
+    this.on('change', this.sort);
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+    this.comparator = field;
+    this.sort();
   }
 
 });
@@ -58,7 +60,7 @@ var MovieView = Backbone.View.extend({
                         </div>'),
 
   initialize: function() {
-    // your code here
+    this.on('change', this.render());
   },
 
   events: {
@@ -66,7 +68,7 @@ var MovieView = Backbone.View.extend({
   },
 
   handleClick: function() {
-    // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -79,7 +81,7 @@ var MovieView = Backbone.View.extend({
 var MoviesView = Backbone.View.extend({
 
   initialize: function() {
-    // your code here
+    this.on('change', this.render());
   },
 
   render: function() {
